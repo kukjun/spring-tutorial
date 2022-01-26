219bb30dd81a9bd42189e03b524f8a93867f2fb5
Kukjun Lee <20171655.info@edu.hanbat.ac.kr>
2022-01-26 21:42:12 +0900

    <feature> : <Upload Chapter08 Transaction>
    * Transactional 어노테이션 적용
    * 적용 후 테스트 완료

---
+++ chapter2/src/main/java/chap08/main/MainForCPS.java
@@ -0,0 +1,27 @@
+package chap08.main;
+
+import chap03.spring.MemberNotFoundException;
+import chap03.spring.WrongIdPasswordException;
+import chap08.config.AppCtx;
+import chap08.spring.ChangePasswordService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainForCPS {
+
+  public static void main(String[] args) {
+    AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
+
+    ChangePasswordService cps = ctx.getBean("changePasswordService", ChangePasswordService.class);
+
+    try {
+      cps.changePassword("kukjun@test.com", "1234", "1111");
+      System.out.println("암호 변경 완료");
+    } catch (MemberNotFoundException e) {
+      System.out.println("회원 데이터가 존재하지 않습니다.");
+    } catch (WrongIdPasswordException e) {
+      System.out.println("암호가 올바르지 않습니다.");
+    }
+
+    ctx.close();
+  }
+}
