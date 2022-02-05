2424c2b34a14bc8614c2674186758c3a2118484d
Kukjun Lee <20171655.info@edu.hanbat.ac.kr>
2022-02-05 19:20:56 +0900

    <feature> : <Upload Chapter11>
    * Chapter11 예제를 작성

--- sp5-chap11/src/main/java/config/ControllerConfig.java
+++ sp5-chap11/src/main/java/config/ControllerConfig.java
@@ -5,14 +5,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import spring.MemberRegisterService;
+import survey.SurveyController;
 
 @Configuration
 public class ControllerConfig {
 
     @Bean
     public RegisterController registerController() {
-        System.out.println("checkController");
+        System.out.println("check RegisterController");
         return new RegisterController();
+    }
+
+    @Bean
+    public SurveyController surveyController() {
+        System.out.println("check SurveyController");
+        return new SurveyController();
     }
 
 }
